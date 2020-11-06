34416e736a7e607ba2f0b5c2ecce6505401624b4
idic779 <idic779@gmail>
2020-11-06 22:26:27 +0800

    add layout

--- app/src/main/java/com/example/curveview/MainActivity.java
+++ app/src/main/java/com/example/curveview/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.curveview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CurveView curveView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        curveView=findViewById(R.id.curve);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curveView.startAnim();
+            }
+        });
     }
 }
